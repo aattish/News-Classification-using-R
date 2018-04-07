529451001e7cd5ddaa3191f0802273c39c6ef85a
aattish <36880980+aattish@users.noreply.github.com>
2018-04-08 00:28:38 +0530

    Add files via upload

---
+++ init.R
@@ -0,0 +1,11 @@
+
+# initialise environment
+
+# load libraries
+library(tm) # text-mining tools
+library(RTextTools) # text-mining tools
+library(wordcloud) # word-cloud
+library(e1071) # naive-bayes
+library(ggplot2) # plot
+library(caret)
+
