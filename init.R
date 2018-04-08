ab2bda55ad2d729312aa13ce366a9316f05f4192
sharmaakshay177 <35631635+sharmaakshay177@users.noreply.github.com>
2018-04-08 12:33:07 +0530

    wordcloud description

--- init.R
+++ init.R
@@ -4,7 +4,7 @@
 # load libraries
 library(tm) # text-mining tools
 library(RTextTools) # text-mining tools
-library(wordcloud) # word-cloud
+library(wordcloud) # word-cloud tells most frequent words/keywords used in paragraph
 library(e1071) # naive-bayes
 library(ggplot2) # plot
 library(caret) #classification and regression traning
